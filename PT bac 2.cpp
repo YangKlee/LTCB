616d29a088249780d9f82fa348468149a4972acb
YangKlee <khanhduong18072005@gmail.com>
2023-10-05 14:19:12 +0700

    Create PT bac 2.cpp

---
+++ PT bac 2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int a,b,c,d;
+	printf("Nhap a, b,c: ");scanf("%d%d%d", &a, &b, &c);
+	d = b*b -4*a*c
+	if (d >0)
+	{
+		printf("Phuong trinh co hai nghiem x1 = %f \nx2 = %f", (-b + sqrt(d))/(2*a))
+	}
+}
