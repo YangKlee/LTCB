884cf2791189f1ec8ecf0994077c17885a6effea
YangKlee <khanhduong18072005@gmail.com>
2023-10-05 18:32:30 +0700

    tramcam

--- bai tap cua Nhi/bai19.c
+++ bai tap cua Nhi/bai19.c
@@ -2,24 +2,31 @@
 #include<math.h>
 int main()
 {
-	double e,cos = 0;
-	int x,n, m_gt = 0;
-	x = 5;
+	double e,coss = 0;
+	long int x,n, m_gt = 1;
+	x = 1;
 	n = 1;
-	e = 7; //e>0
+	e = 15; //e>0
 	while(1)
 	{
-		for(int i = 1; 2*n <=i; i++ )
+		m_gt = 1;
+		//printf("n= %d\n", n);
+		for(int i = 1; i <= (2*n); i++ )
 		{
 			m_gt = m_gt*i;
+			//printf("%d\n", m_gt);
 		}
-		cos += (pow(-1,n)*(pow(x,2*n)/(m_gt)));
-		if(fabs(pow(-1,n))*(pow(x,2*n)/(m_gt)) > e )
+		coss += (pow(-1,n)*(pow(x,2*n)/(m_gt)));
+		
+		
+		if(fabs((pow(-1,n)*(pow(x,2*n)/(m_gt)))) > e )
 		{
 			break;
 		}
+		n++;
+
 	
 		
 	}
-	printf("%f", cos);
+	printf("%.20lf", coss);
 }
