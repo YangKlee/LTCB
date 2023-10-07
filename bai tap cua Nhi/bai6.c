e3021688369201550fdf8e45e30ec44a6167db78
YangKlee <khanhduong18072005@gmail.com>
2023-10-07 22:43:49 +0700

    bai 6 chua code xong

---
+++ bai tap cua Nhi/bai6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int n,k,i,gt1,gt2,gt3;
+	printf("Nhap giai thua chap k cua n");scanf("%d%d", &n, &k);
+	if(n>0 && k >0 && k<= n)
+	{
+		(n = 0)? gt1 = 1: 
+	}
+}
