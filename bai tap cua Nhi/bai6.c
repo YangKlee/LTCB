44db1afde7b4cb1cfe0f55f4668561b277cbac6e
YangKlee <khanhduong18072005@gmail.com>
2023-10-08 10:57:40 +0700

    bai 6 complete

--- bai tap cua Nhi/bai6.c
+++ bai tap cua Nhi/bai6.c
@@ -2,10 +2,18 @@
 #include<math.h>
 int main()
 {
+	//cho kieu long long de tranh bi tran voi n>=10
+	// pham vi kieu du lieu: 0 to 2^64-1
 	unsigned long long int gt1 = 1,gt2 = 1,gt3 = 1;
-	int n,k,i,gt_ck;
+	/*
+	Tach ra cho de tinh 
+	gt1 = n!
+	gt2 = k!
+	gt3 = (n-k)!
+	*/
+	int n,k,gt_ck;
 	printf("Nhap giai thua to hop chap k cua n: ");scanf("%d%d", &k, &n);
-	if(n>0 && k >0 && k<= n)
+	if(n>0 && k >0 && k<= n) // dieu kien de to hop xac dinh
 	{
 		for(int i = 1; i <= n; i++)
 		{
@@ -24,7 +32,7 @@ int main()
 	}
 	else
 	{
-		if(n ==0)
+		if(n ==0) // Ngoai le: neu n = 0 thi giai thua se bang 1 voi moi k
 		{
 			printf("Giai thua chap k cua n la: %d", 1);
 		}
@@ -34,4 +42,10 @@ int main()
 		}
 		
 	}
+	/*
+	Cac bug con ton tai:
+	- Voi n > 20 thi se bi tran du lieu => chua khac phuc
+	Bonus: Cho kieu du lieu unsigned long long int van khong khac phuc duoc, nguyen nhan tran do nhan hai so qua lon voi nhau
+	
+	*/
 }
