e979a753089c37c3daeb5f707675ce7562b5cdf8
YangKlee <khanhduong18072005@gmail.com>
2023-10-08 10:49:11 +0700

    ehe

--- bai tap cua Nhi/bai6.c
+++ bai tap cua Nhi/bai6.c
@@ -2,10 +2,36 @@
 #include<math.h>
 int main()
 {
-	int n,k,i,gt1,gt2,gt3;
-	printf("Nhap giai thua chap k cua n");scanf("%d%d", &n, &k);
+	unsigned long long int gt1 = 1,gt2 = 1,gt3 = 1;
+	int n,k,i,gt_ck;
+	printf("Nhap giai thua to hop chap k cua n: ");scanf("%d%d", &k, &n);
 	if(n>0 && k >0 && k<= n)
 	{
-		(n = 0)? gt1 = 1: 
+		for(int i = 1; i <= n; i++)
+		{
+			gt1 *= i;
+		}
+		for(int i = 1; i <= k;i++)
+		{
+			gt2 *=i;
+		}
+		for(int i = 1; i <= (n-k); i++)
+		{
+			gt3 *= i;
+		}
+		gt_ck = (gt1/(gt2*gt3));
+		printf("Giai thua chap k cua n la: %d", gt_ck);
+	}
+	else
+	{
+		if(n ==0)
+		{
+			printf("Giai thua chap k cua n la: %d", 1);
+		}
+		else
+		{
+			printf("Deo tinh duoc");
+		}
+		
 	}
 }
--- bai tap cua Nhi/tinhgt.cpp
+++ bai tap cua Nhi/tinhgt.cpp
@@ -1,9 +1,18 @@
-#include<stdio.h>
-#include<math.h>
-int main()
-{
-	int n = 5;
-	n = pow(5,2);
+#include <stdio.h>
 
-	printf("%d", n);
+int main() {
+  unsigned long long int gt1 = 1, temp = 0;
+  for (int i = 1; i <= 30; i++) {
+    gt1 *= i;
+    if(i == 30/10)
+    {
+    	
+    	temp += gt1;
+    	gt1 = 1;
+	}
+	
+  }
+  gt1 *= temp;
+  printf("%llu\n", gt1);
+  return 0;
 }
